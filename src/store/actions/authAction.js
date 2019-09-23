a2ab3c3d291ccb75380f55634430083124e95799
Jojo Zhang <jojozhang0327@gmail.com>
2019-09-23 18:00:02 -0400

    complete nav bar, and store setup

---
+++ src/store/actions/authAction.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGOUT = 'LOGOUT';
+export const REGISTER_START = 'REGISTER_START ';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAILURE = 'REGISTER_FAILURE';
+
+// login actions
+export const login = (credentials, history) => dispatch => {
+  dispatch({ type: LOGIN_START })
+  axios
+    .post('/auth/login', credentials)
+    .then(res => {
+      console.log(res)
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+      // set token to local storage
+      localStorage.setItem('token', res.data.token);
+      localStorage.setItem('user_id', res.data.id);
+      // route to dashboard
+      history.push('/dashboard');
+    })
+    .catch(err => {
+      dispatch({ type: LOGIN_FAILURE, payload: err.response })
+      console.log('authFailure', err.response)
+    });
+};
+
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
+  localStorage.removeItem('token');
+  localStorage.removeItem('user_id');
+}
+
+// sign up actions
+export const register = (user, history) => dispatch => {
+  dispatch({ type: REGISTER_START });
+  axios
+    .post('/auth/register', user)
+    .then(res => {
+      dispatch({ type: REGISTER_SUCCESS })
+      history.push('/login');
+    })
+    .catch(err => {
+      dispatch({ type: REGISTER_FAILURE, payload: err.response })
+    })
+} 
---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,64 @@
+import {
+  LOGIN_START,
+  LOGIN_SUCCESS, 
+  LOGIN_FAILURE, 
+  LOGOUT,
+  REGISTER_START,
+  REGISTER_SUCCESS, 
+  REGISTER_FAILURE
+} from '../actions/authAction';
+
+const initialState = {
+  error: '',
+  isLoading: false,
+  isAuth: localStorage.getItem('token') ? true : false,
+};
+
+const authReducer = (state=initialState, action) => {
+  switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        error: '',
+        isLoading: true,
+      }
+    case LOGIN_SUCCESS: 
+      return {
+        ...state,
+        isLoading: false,
+        isAuth: true,
+      }
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      }
+    case LOGOUT:
+      return {
+        ...state,
+        error: ''
+      }
+    case REGISTER_START:
+      return {
+        ...state,
+        error: '',
+        isLoading: true,
+      }
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+      }
+    case REGISTER_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      }
+    default:
+      return state;
+  }
+}
+
+export default authReducer;

3959aa48687789264067e9b8bbf4574b752153c2
richanynguon <alina.nguon@gmail.com>
2019-09-24 11:05:48 -0400

    Changing output for the card

--- src/components/CampaignCard.js
+++ src/components/CampaignCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-export default function CampaignCard() {
+export default function CampaignCard({campaign}) {
   return (
     <>
-    <p>Campaign:</p>
-    <p>Category:</p>
-    <p>Monetary Goal:</p>
-    <p>Duration:</p>
-    <p>Country:</p>
-    <p>Status:</p>
+    <p>Campaign:{campaign.campaignName}</p>
+    <p>Category:{campaign.categories}</p>
+    <p>Monetary Goal:{campaign.monetaryGoal}</p>
+    <p>Duration:{campaign.duration}</p>
+    <p>Country:{campaign.country}</p>
+    <p>Status:{campaign.status}</p>
     <Link to="#">
         <button>
         Campaign Details

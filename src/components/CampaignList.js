3067e14794164612274ca692a97d78982f86805e
richanynguon <alina.nguon@gmail.com>
2019-09-24 05:32:16 -0400

    Create Campgin List.js

---
+++ src/components/CampaignList.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import CampaginCard from './CampaignCard';
+import axios from 'axios';
+
+function CampaignList(){
+    const [campaignList, setCampaignList] = useState([]);
+
+    useEffect()
+    return(
+        <>
+        </>
+    );
+};
+
+export default CampaignList;

9ed6e2e70f7dce466724414c5491423dbefe32b4
richanynguon <alina.nguon@gmail.com>
2019-09-24 21:57:08 -0400

    Finish Skeleton of Results Page

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,47 +1,88 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 
 const output = {
-    'results': 'int(result[0])',
-    'custom_stats': {
-        1: 'custom_results1',
-        2: 'custom_results2',
-        3: 'custom_results3',
-        4: 'custom_results4',
-        5: 'custom_results5',
-        6: 'custom_results6'
-    }
-}
-
-function Results(){
-    const [results, setResults] = useState({});
-    
-    useEffect(()=>{
-        // axios.get('#')
-        // .then((res)=>{
-        //     setResults(res.data)
-        // })
-        // .catch((err)=>{
-        //     console.log(err)
-        // })
-        setResults(output)
-    },[]);
-
-// these are the questions we will answer 
-// # {####} of campaigns raising more than {persons amount} have been successful
-// # {##}% of campaigns in {persons category} are successful
-// # campaigns in {persons category} raise an average of {$$$} successfully
-
-// # successful campaigns raising over {persons amount} have an average duration of {##} days
-// # campaigns raising (persons amount) have an average number of {####} backers
-// # successful campaigns in {persons category} raise an average of {$$$$} above their goal
-    
-    return(
-        <>
-        <div></div>
-
-        </>
-    );
+	results: "int(result[0])",
+	custom_stats: {
+		1: "custom_results1",
+		2: "custom_results2",
+		3: "custom_results3",
+		4: "custom_results4",
+		5: "custom_results5",
+		6: "custom_results6"
+	}
 };
+
+const Data = [
+    {
+        "id": 1,
+        "user_id": 1,
+        "campaignName": "Test_Project_1",
+        "categories": "Games",
+        "description": "Put the decription here and bla bla bla.",
+        "monetaryGoal": 100000,
+        "duration": 30,
+        "country": "USA"
+    },
+]
+function Results() {
+    const [results, setResults] = useState({});
+    const [campaign, setCampaign] = useState([]);
+
+	useEffect(() => {
+        // axios.all([
+        //     axios.get('#'),
+        //     axios.get('#')
+        //   ])
+        //   .then(axios.spread((resultsData, campaignData) => {
+        //     setResults(resultsData)
+        //     setCampaign(campaignData)
+        //   }));
+          
+        setResults(output.custom_stats);
+        setCampaign(Data[0]);
+	}, []);
+
+	return (
+		<>
+			{/* {
+            Object.keys(results).map((detail)=>(
+                <div>{results[detail]}</div>
+            ))
+        } */}
+			<div>
+				<p>
+                {results[1]} of campaigns raising more than {campaign.monetaryGoal} have been successful
+                </p>
+			</div>
+            <div>
+				<p>
+                {results[2]}% of campaigns in {campaign.categories} are successful
+                </p>
+			</div>
+            <div>
+				<p>
+                Campaigns in {campaign.categories} raise an average of ${results[3]} successfully    
+                </p>
+			</div>
+            <div>
+				<p>
+                Successful campaigns raising over {campaign.monetaryGoal} have an average duration of {results[4]} days
+                </p>
+			</div>
+            <div>
+				<p>
+                Campaigns raising {campaign.monetaryGoal} have an average number of {results[5]} backers 
+                </p>
+			</div>
+            <div>
+				<p>
+                Successful campaigns in {campaign.categories} raise an average of {results[6]} above their goal
+                </p>
+			</div>
+		</>
+	);
+}
 
 export default Results;

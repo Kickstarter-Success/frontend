2ac83a289e0ec8507f982c913c7c2b2445fbd919
Jojo Zhang <jojozhang0327@gmail.com>
2019-09-24 22:50:26 -0400

    fix campaign card

---
+++ src/components/CampaignCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+export default function CampaignCard({campaign}) {
+  return (
+    <>
+    <p>Campaign:{campaign.campaignName}</p>
+    <p>Category:{campaign.categories}</p>
+    <p>Monetary Goal:{campaign.monetaryGoal}</p>
+    <p>Duration:{campaign.duration}</p>
+    <p>Country:{campaign.country}</p>
+    <p>Status:</p>
+    <Link to="#">
+        <button>
+        Campaign Details
+        </button>
+    </Link>
+    </>
+  );
+}
